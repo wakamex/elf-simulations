782e29888fc4c628211967fe83ccd418d3ec9884
mihai6 <mihai@element.fi>
2022-12-05 21:21:55 -0500

    accurately calculate effective rate

--- src/elfpy/user.py
+++ src/elfpy/user.py
@@ -36,10 +36,12 @@ class AgentWallet:
 
     def __post_init__(self):
         """Post initialization function"""
-        # check if this represents a trade (one side will be negative)
-        total_tokens = sum(list(self.token_in_protocol.values()))
-        if (self.base_in_wallet < 0 or total_tokens < 0) and total_tokens != 0:
-            self.effective_price = self.base_in_wallet / total_tokens
+        total_bonds = sum(list(self.token_in_protocol.values())) + sum(list(self.token_in_wallet.values()))
+        total_base = sum(list(self.base_in_protocol.values())) + self.base_in_wallet
+        # in trades, one side will be neegative, and neither side can be zero
+        this_is_a_trade = (total_base < 0 or total_bonds < 0) and total_bonds != 0
+        if this_is_a_trade and self.stretched_time_remaining != 0:
+            self.effective_price = abs(total_base / total_bonds)
             self.effective_rate = calc_apr_from_spot_price(self.effective_price, self.stretched_time_remaining)
 
     def __getitem__(self, key):
@@ -52,7 +54,8 @@ class AgentWallet:
         output_string = ""
         for key, value in vars(self).items():
             if value:  #  check if object exists
-                if value != 0 and value is not None and key not in ["stretched_time_remaining"]:
+                vars_not_to_display = ["stretched_time_remaining"]
+                if value != 0 and value is not None and key not in vars_not_to_display:
                     output_string += f" {key}: "
                     if isinstance(value, float):
                         if key in ["effective_price", "effective_rate"]:
