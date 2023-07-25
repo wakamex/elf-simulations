40a7e99a47baf88a438a87c7a9bfc2288cdf32f5
Mihai <mcosma@gmail.com>
2023-07-25 13:21:10 -0400

    make dbots shorter

--- examples/eth_bots/custom_policies/dbot.py
+++ examples/eth_bots/custom_policies/dbot.py
@@ -1,71 +1,33 @@
 """Example custom agent strategy."""
 from __future__ import annotations
 
-import logging
-from typing import TYPE_CHECKING
-
 from fixedpointmath import FixedPoint
+from numpy.random._generator import Generator as NumpyGenerator
 
 from elfpy.agents.policies import BasePolicy
 from elfpy.markets.hyperdrive import HyperdriveMarketAction, MarketActionType
 from elfpy.types import MarketType, Trade
+from elfpy.wallet.wallet import Wallet
 
-if TYPE_CHECKING:
-    from numpy.random._generator import Generator as NumpyGenerator
+# pylint: disable=too-few-public-methods, missing-function-docstring, invalid-name, too-many-arguments, line-too-long, missing-class-docstring
+LP, CLOSE_LP, LONG, SHORT, CLOSE_LONG, CLOSE_SHORT = "add_liquidity", "remove_liquidity", "open_long", "open_short", "close_long", "close_short"
 
-    from elfpy.markets.hyperdrive import HyperdriveMarket
-    from elfpy.wallet.wallet import Wallet
-
-# pylint: disable=too-few-public-methods, missing-function-docstring, invalid-name
-
-
-def long_action(amount: int, wallet):
-    return HyperdriveMarketAction(MarketActionType.OPEN_LONG, trade_amount=FixedPoint(amount), wallet=wallet)
-
-
-def short_action(amount: int, wallet):
-    return HyperdriveMarketAction(MarketActionType.OPEN_SHORT, trade_amount=FixedPoint(amount), wallet=wallet)
-
-
-def LP_action(amount: int, wallet):
-    return HyperdriveMarketAction(MarketActionType.ADD_LIQUIDITY, trade_amount=FixedPoint(amount), wallet=wallet)
-
-
-def long(amount: int, wallet) -> Trade:
-    return Trade(market=MarketType.HYPERDRIVE, trade=long_action(amount, wallet))
-
-
-def short(amount: int, wallet) -> Trade:
-    return Trade(market=MarketType.HYPERDRIVE, trade=short_action(amount, wallet))
-
-
-def LP(amount: int, wallet):
-    return Trade(market=MarketType.HYPERDRIVE, trade=LP_action(amount, wallet))
-
+def new_trade(market: MarketType, action_type: str, amount: FixedPoint | int, wallet: Wallet, mint_time=None) -> Trade:
+    amount = amount if isinstance(amount, FixedPoint) else FixedPoint(amount)
+    action = HyperdriveMarketAction(MarketActionType(action_type), wallet, amount, FixedPoint(0), mint_time)
+    return Trade(market=market, trade=action)
 
 class DBot(BasePolicy):
-    """Deterministic bot."""
-
-    def __init__(self, budget: FixedPoint, rng: NumpyGenerator | None = None, trade_amount: FixedPoint | None = None):
-        if trade_amount is None:
-            self.trade_amount = FixedPoint(100)
-            logging.warning("Policy trade_amount not set, using 100.")
-        else:
-            self.trade_amount: FixedPoint = trade_amount
+    def __init__(self, budget: FixedPoint, rng: NumpyGenerator | None = None, amount: FixedPoint | None = None):
+        self.amount = FixedPoint(100) if amount is None else amount
         self.trade_count = 0
-        self.trade_list = [(LP, 100), (long, 100), (short, 100)]
+        self.trade_list = [(LP, 100), (LONG, 100), (SHORT, 100), (CLOSE_SHORT, 100)]
         super().__init__(budget, rng)
 
-    def action(self, market: HyperdriveMarket, wallet: Wallet) -> list[Trade]:
-        """Pick a trade from the pre-determined list based on trade_count.
-
-        Returns
-        -------
-        list[MarketAction]
-            list of actions
-        """
+    def action(self, market: MarketType, wallet: Wallet) -> list[Trade]:
         if self.trade_count >= len(self.trade_list):
             return []
-        trade, amount = self.trade_list[self.trade_count]
+        action, amount = self.trade_list[self.trade_count]
+        mint_time = list(wallet.longs)[0] if action == CLOSE_LONG else list(wallet.shorts)[0] if action == CLOSE_SHORT else None
         self.trade_count += 1
-        return [trade(amount, wallet)]
+        return [new_trade(market=market, action_type=action, amount=amount, wallet=wallet, mint_time=mint_time)]
