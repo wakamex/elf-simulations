520ec757ac9bd37b7452a3d0f98521211e233c0b
Mihai <mcosma@gmail.com>
2023-09-14 14:02:54 -0400

    add back calc_fixed_rate that works on dataframes

--- lib/chainsync/chainsync/analysis/calc_fixed_rate.py
+++ lib/chainsync/chainsync/analysis/calc_fixed_rate.py
@@ -1,11 +1,14 @@
 """Calculate the fixed interest rate."""
 from decimal import ROUND_DOWN, ROUND_UP, Decimal, localcontext
 
+import numpy as np
 import pandas as pd
 
+from .calc_spot_price import calc_spot_price
 
 def calc_fixed_rate(spot_price: pd.Series, position_duration: Decimal):
-    """Calculates the fixed rate given trade data."""
+    """Calculate fixed rate from spot price."""
+    # sourcery skip: inline-immediately-returned-variable
     # Position duration (in seconds) in terms of fraction of year
     # This div should round up
     # This replicates div up in fixed point
@@ -21,3 +24,18 @@ def calc_fixed_rate(spot_price: pd.Series, position_duration: Decimal):
         ctx.rounding = ROUND_DOWN
         fixed_rate = (1 - spot_price) / (spot_price * annualized_time)  # type: ignore
     return fixed_rate
+
+def calc_fixed_rate_df(trade_data, config_data):
+    """Calculate fixed rate from trade and config data."""
+    trade_data["rate"] = np.nan
+    annualized_time = config_data["positionDuration"] / Decimal(60 * 60 * 24 * 365)
+    spot_price = calc_spot_price(
+        trade_data["share_reserves"],
+        trade_data["bond_reserves"],
+        config_data["initialSharePrice"],
+        config_data["invTimeStretch"],
+    )
+    fixed_rate = (Decimal(1) - spot_price) / (spot_price * annualized_time)
+    x_data = trade_data["timestamp"]
+    y_data = fixed_rate
+    return x_data, y_data
